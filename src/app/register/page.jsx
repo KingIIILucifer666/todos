4067df91db22993576f4f324db8feb42996584db
sannanhaider <sannanhaider5@gmail.com>
2025-01-06 12:56:51 +0500

    minor bug

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -113,7 +113,7 @@ export default function RegisterPage() {
 
         <p className="mt-4 text-center text-sm text-gray-600">
           Already have an account?{" "}
-          <a href="/login" className="text-blue-500 hover:underline">
+          <a href="/" className="text-blue-500 hover:underline">
             Login
           </a>
         </p>
